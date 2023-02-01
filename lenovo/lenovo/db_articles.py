0a2f0ddccee3c03ae7f248e72c9e8db4146fa54b
naira34 <116266547+naira34@users.noreply.github.com>
2023-02-01 21:55:26 +0100

    n

---
+++ lenovo/lenovo/db_articles.py
@@ -0,0 +1,17 @@
+articles=[
+    {
+       " titre":"articles1",
+       "date_pub":"11/11/2022",
+       "contenu":"enfants",
+    },
+ {
+       " titre":"articles2",
+       "date_pub":"11/11/2022",
+       "contenu":"scientifique",
+    },
+ {
+       " titre":"articles3",
+       "date_pub":"11/11/2022",
+       "contenu":"adultes",
+    }
+]
--- lenovo/lenovo/settings.py
+++ lenovo/lenovo/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-tmplate=os.path.join(BASE_DIR,'tmplate')
+
 
 
 
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+  
 ]
 
 MIDDLEWARE = [
